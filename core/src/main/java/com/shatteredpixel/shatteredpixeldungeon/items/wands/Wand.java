520b46e840252f56df7bdf2d802e3c0228b8f046
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-03-25 04:25:32 +0300

    wands recharge slower

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/Wand.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/Wand.java
@@ -570,9 +570,9 @@ public abstract class Wand extends Item {
 	
 	public class Charger extends Buff {
 		
-		private static final float BASE_CHARGE_DELAY = 5f;
-		private static final float SCALING_CHARGE_ADDITION = 20f;
-		private static final float NORMAL_SCALE_FACTOR = 0.435f;
+		private static final float BASE_CHARGE_DELAY = 8f;
+		private static final float SCALING_CHARGE_ADDITION = 30f;
+		private static final float NORMAL_SCALE_FACTOR = 0.300f;
 
 		private static final float CHARGE_BUFF_BONUS = 0.25f;
 
